7f64c2f449cfee3877cb2c44265fb91c1c7fa162
Alvin <AlvinWang627@gmail.com>
2024-01-15 00:16:28 +0800

    feat: add table

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import UserInput from "./components/UserInput";
+import Results from "./components/Results";
 function App() {
-  return <h1>React Investment Calculator</h1>;
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+  const validInput = userInput.duration >= 1;
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+  return (
+    <>
+      <Header />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      {!validInput && <p className="center">"please input valid input"</p>}
+      {validInput && <Results input={userInput} />}
+    </>
+  );
 }
 
 export default App;
